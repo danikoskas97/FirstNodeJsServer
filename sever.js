6370cc2794ceacb725f980f2c522833d1c0adfdb
dani koskas <koskas.dani@gmail.com>
2020-05-25 14:28:35 +0300

    first commit

--- sever.js
+++ sever.js
@@ -1,24 +1,16 @@
 
 let http  = require('http') // ouvrir server
 let fs = require('fs')
-let url = require(url)
-
-
+let url = require('url')
 let server = http.createServer() //creation du server
 
 server.on('request', (request, response) => { // prend la requête et la réponse
 
     response.writeHead(200)
-
-    console.log(request.url)
-
-    response.end('bonjour')
-
-
-
-
-    /*
-    fs.readFile('index.html', (err, data) => { // affiche le fichier index.html 
+    let query = url.parse(request.url, true).query
+    let name = query.name === undefined ? 'annonyme' : query.name
+    
+    fs.readFile('index.html', 'utf-8', (err, data) => { // affiche le fichier index.html 
         if (err)  {
             response.writeHead(404)
             response.end('Ce fichier existe pas')
@@ -27,13 +19,11 @@ server.on('request', (request, response) => { // prend la requête et la répons
                 'Content-type': 'text/html; charset=utf-8' //encodage utf-8
             
             })
+            data = data.replace('{{ name }}', name)
             response.end(data)
         }
     })
-    */
+
 })
 server.listen(8080)
-
-
-
 
